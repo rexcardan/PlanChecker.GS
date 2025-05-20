97b12a9816786573839a57d4b481e40477a6ba94
GatewayD1 <GatewayD1@ams.demo.cloud.varian.com>
2025-05-20 12:06:58 -0400

    looks to be working

--- ESAPIX_StarterUI1/ViewModels/MainViewModel.cs
+++ ESAPIX_StarterUI1/ViewModels/MainViewModel.cs
@@ -12,6 +12,8 @@ using System.Windows;
 using ESAPIX.Extensions;
 using ESAPIX.Constraints.DVH;
 using System.Collections.ObjectModel;
+using ESAPIX_WPF_Example.CustomConstraints;
+using VMS.TPS.Common.Model.Types;
 
 namespace ESAPX_StarterUI.ViewModels
 {
@@ -50,9 +52,10 @@ namespace ESAPX_StarterUI.ViewModels
             Constraints.AddRange(new ConstraintResultPair[]
             {
                 new ConstraintResultPair(ConstraintBuilder.Build("PTV45", "Max[%] <= 110")),
-                new ConstraintResultPair(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
+                new ConstraintResultPair(ConstraintBuilder.Build("Rectum", "V75Gy[cc] <= 15")),
                 new ConstraintResultPair(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
                 new ConstraintResultPair(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
+                new ConstraintResultPair(new MaxgEUDConstraint("Rectum", new DoseValue(60, DoseValue.DoseUnit.Gy), 40)),
               //  new PlanConstraint(new CTDateConstraint())
             });
         }
